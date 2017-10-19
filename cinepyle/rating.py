2e792baaaae19eebfd3d64c2829c7ee4251197a6
iacopo breschi <iacopobreschi@gmail.com>
2017-10-19 23:56:42 +0200

    [Refactoring] better rating interface

--- cinepyle/rating.py
+++ cinepyle/rating.py
@@ -3,158 +3,95 @@ Created on Jun 19, 2017
 
 @author: ibreschi
 '''
-
+from . import utils
 from imdb import IMDb
 
-from best_activities import RatingAssigner
-from best_activities.utils import cached
-
-# @cached('directors_cache.pickle')
-def load_directors(films):
-        director_films = {}
-        directors = set()
-        for film in films:
-            directors.add(film.director)
-        
-        
-        ia = IMDb()
-        pbar = ProgressBar(len(directors))
-        for name in directors:
-            print '\tAquiring informations from IMDb about :', name
-            directors_with_one_name = ia.search_person(name) 
-            if len(directors_with_one_name) == 0:
-                print '\t', name, ' not found in IMDb'
-                continue
-            
-            # !!!!!! We suppose that the first in the list is allways the one we are looking for.
-            
-            director = directors_with_one_name[0]
-            ia.update(director)
-            films = []
-            if director.data.has_key('director'):
-                films += director[u'director']
-
-            director_films[name] = films
-            pbar.progress()
-        return director_films
-
-
-def search_single(film_name, director_name):
-    
-    if director_name == '':
-        return (0, 0)
-    
-    ia = IMDb()
-    film_list = ia.search_movie(film_name)
-    print '\tSearching', film_name, 'directed by:', director_name
-    for film in film_list:
-        ia.update(film)
-        if film.data.has_key('director'):
-            # take the first director
-            director = film['director'][0]
-            if same_name(director['name'], director_name) and film.data.has_key('rating'):
-                return (film['rating'] , film['votes'])
-            else :
-                print '\t\tOther director found:', director , 'continuing..'
-
-    return (0, 0)
-
-
-def assign_IMDb_ratings(films, directorToFilm_map):
-    ia = IMDb()
-       
-    print '## We have', len(films), 'films to rate!'
-    pbar = ProgressBar(len(films))
-    
-    for cinem_film in films:
-        l_films = []
-        if directorToFilm_map.has_key(cinem_film.director):
-            l_films = directorToFilm_map[cinem_film.director]
-        
-        # Search the films in the director films that have a name similar to the one we retreived from the cinematheque.
-        l_result = filter(lambda x: x if (similar(x['title'], cinem_film.name) > 0.7) else None, l_films)
-        if len(l_result) > 1:
-            # take the most similar one
-            l_result = [reduce(lambda a, b: a if (similar(a['title'], cinem_film.name) > similar(b['title'], cinem_film.name)) else b, l_result)]
-        if len(l_result) == 0:
-            # no film found in director's film list continue to next director in director list
-            # Try again by looking by movie name and match it with the director
-            rating, votes = search_single(cinem_film.name, cinem_film.director)
-            if rating == 0:
-                print '\t\tNo film named', cinem_film.name , 'directed by', cinem_film.director, 'found on IMDb. Assigning: 6.5'
-                rating = 6.5
-                votes = 1
-            cinem_film.setValue(rating)
-            cinem_film.setVotes(votes)
-        else :
-            # one film found!
-            film = l_result[0]
-            ia.update(film)
-            # print 'Imdb Rating for film:', film , 'is :', film['rating']
-            if film.data.has_key('rating'):
-                rating = float(film['rating'])
-                votes = str(film['votes'])
-            else:
-                print '\t\tThe film named', cinem_film.name, 'directed by', cinem_film.director, 'not rated in IMDb! Assigning: 6.5'
-                rating = 6.5
-                votes = 1
-            cinem_film.setValue(rating)
-            cinem_film.setVotes(votes)
-
-        pbar.progress()
-
-
-
-class MoviesRatingAssigner(RatingAssigner):
+class MovieRatingAssigner(object):
     """
     Implementation of RatingAssigner.
     """
 
-    def __init__(self, films, force_rating = False, refresh_directors = False):
-        self.force_rating = force_rating
-        self.films = films
-        self.director_films = load_directors(films)
-        
-    def rate(self):
-        ia = IMDb()
-        print '## We have', len(self.films), 'films to rate!'
-        pbar = ProgressBar(len(self.films))
+    def __init__(self):
+        self.ia = IMDb()
+        self.director_to_films = {}
     
-        for film in self.films:
+    def fetch_movie_by_name(self, film_name, director_name):
+        if director_name == '' or film_name == '':
+            return None
+        print '\tSearching', film_name, 'directed by:', director_name
+
+        fetched_films = self.ia.search_movie(film_name)
+    
+        for film in fetched_films:
+            self.ia.update(film)
+            if film.data.has_key('director'):
+                # take the first director
+                director = film['director'][0]
+                if utils.same_name(director['name'], director_name) and film.data.has_key('rating'):
+                    return film
+                else :
+                    print '\t\tOther director found:', director , 'continuing..'
+        return None
+    
+    @utils.with_pickle
+    def fetch_directors(self, directors):
+        director_to_films = {}
+        directors = set(directors)       
+        pbar = utils.ProgressBar(len(directors))
+        for name in directors:
+            print '\tAquiring informations about:', name
+            directors_with_one_name = self.ia.search_person(name) 
+            if len(directors_with_one_name) == 0:
+                print '\t', name, ' not found in IMDb'
+                continue
+            
+            director = max(directors_with_one_name, key=lambda x: utils.similar(x['name'], name) )
+            self.ia.update(director)
             films = []
-            if film.director in self.director_films:
-                films = self.director_films[film.director]
-        
-            # Search the films in the director films that have a name similar to the one we retreived from the cinematheque.
-            result = filter(lambda x: x if (similar(x['title'], film.name) > 0.7) else None, films)
-            if len(l_result) > 1:
-                # take the most similar one
-            l_result = [reduce(lambda a, b: a if (similar(a['title'], cinem_film.name) > similar(b['title'], cinem_film.name)) else b, l_result)]
-        if len(l_result) == 0:
-            # no film found in director's film list continue to next director in director list
-            # Try again by looking by movie name and match it with the director
-            rating, votes = search_single(cinem_film.name, cinem_film.director)
-            if rating == 0:
-                print '\t\tNo film named', cinem_film.name , 'directed by', cinem_film.director, 'found on IMDb. Assigning: 6.5'
-                rating = 6.5
-                votes = 1
-            film.setValue(rating)
-            film.setVotes(votes)
-        else :
-            # one film found!
-            film = l_result[0]
-            ia.update(film)
-            # print 'Imdb Rating for film:', film , 'is :', film['rating']
-            if film.data.has_key('rating'):
-                rating = float(film['rating'])
-                votes = str(film['votes'])
-            else:
-                print '\t\tThe film named', cinem_film.name, 'directed by', cinem_film.director, 'not rated in IMDb! Assigning: 6.5'
-                rating = 6.5
-                votes = 1
-            cinem_film.setValue(rating)
-            cinem_film.setVotes(votes)
+            if director.data.has_key('director'):
+                films += director[u'director']
 
-        pbar.progress()
-
+            director_to_films[name] = films
+            pbar.progress()
+        return director_to_films
     
+    def rate_one(self, film):    
+        imdb_film = None
+        rating = 6.5
+        votes = 1
+        print "\tRating:", film.name,"by:", film.director,"..."
+        #1) try to find if the we have already fetched the director
+        if film.director in self.director_to_films:
+            director_films = self.director_to_films[film.director]
+            if len(director_films) > 0: 
+                imdb_film = max(director_films, key=lambda x: utils.similar(x['title'], film.name) )
+        
+        #2) If nothing acceptable has been found try to fetch the single movie
+        if imdb_film is None or utils.similar(imdb_film['title'], film.name) <= 0.7:
+            # Try again by looking by movie name and match it with the director        
+            imdb_film = self.fetch_movie_by_name(film.name, film.director)
+            if imdb_film is None:
+                print '\t\tNo film named', film.name , 'directed by', film.director, 'found on IMDb. Assigning: 6.5'
+        else:
+            self.ia.update(imdb_film)
+            # print 'Imdb Rating for film:', film , 'is :', film['rating']
+            if imdb_film.data.has_key('rating'):
+                rating = float(imdb_film['rating']) 
+                votes = str(imdb_film['votes'])
+            else:
+                print '\t\tThe film named', film.name, 'directed by', film.director, 'not rated in IMDb! Assigning: 6.5'
+        film.setValue(rating)
+        film.setVotes(votes)  
+
+@utils.with_pickle
+def assign_movie_rating(seances):
+    assigner = MovieRatingAssigner() 
+    assigner.fetch_directors([show.director for show in seances])
+    rated_films = []
+    print '## We have', len(seances), 'films to rate!'
+    pbar = utils.ProgressBar(len(seances))
+    for film in seances:
+        assigner.rate_one(film)
+        rated_films.append(film)
+        pbar.progress()
+    return rated_films
