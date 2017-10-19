0bf5b62d2d5435566d62b1740f0c89a196e8fbaf
iacopo breschi <iacopobreschi@gmail.com>
2017-10-20 01:17:47 +0200

    working cinepile use file

--- bin/make_film_raccomandations.py
+++ bin/make_film_raccomandations.py
@@ -4,9 +4,10 @@ Created on Jun 16, 2016
 @author: fean0r
 '''
 
-from cinepyle import CalendarAdapters
-# from cinepylia.rating import MoviesRatingAssigner
-# import opchoice
+from cinepyle import calendar
+from cinepyle import cinematheque
+from cinepyle import rating
+from opchoice import scheduler
 
 import datetime
 
@@ -15,43 +16,36 @@ def __main__():
     start = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
     end = datetime.datetime.strptime('2017-12-31','%Y-%m-%d').isoformat() + 'Z'
     
-    
     # Fill the Agenda with your events
-    personal_calendar = CalendarAdapters.GoogleCalendarAdapter()
+    personal_calendar = calendar.CalendarManager()
     events = personal_calendar.retreiveEvents(start, end)
-    #for event in events:
-    #    print(event)
 
-    
     # Retrive the activities 
-    cine_cal = CalendarAdapters.CinemathequeCalendarAdapter()
-    seances = cine_cal.retreiveSeances(start, end)
-    for seance in seances:
-        print(seance)
+    seances = cinematheque.retreive_seances(start, end)
+    print seances
+    
+    # Remove from the seances the overlapping events :    
+    #personal_calendar.removeOverlappingEvents(seances)
         
-    #cine_cal.remove_overlapping_events(my_cal.events())
-    
-    
-    #cinematheque_retreiver = CinematequeFilmRetriever(cinematheque_urls)
-    #cinematheque_shows = cinematheque_retreiver.retreive()
-    
-    # Give Rating to activities :
-    #MoviesRatingAssigner(cine_cal.events())
+    # Rate to activities :
+    rated_seances = rating.assign_movie_rating(seances)
+    print rated_seances
     
     # Set the decision constraints 
-#     constraints = opchoice.SchedulerConstraints()
-#     constraints.add_rating_function(MoviesRatingAssigner)
-#     constraints.add_time_constraints(personal_calendar)
-#     constraints.set_max_activity_by_day = 1
-#     constraints.set_max_activity_by_week = 3
-#     constraints.add_activity_to_avoid("")
-#     constraints.add_activity_to_perfom("")
-#     
-#     # Get the best cinematheque_shows with my agenda and decision params
-#     decision_maker = opchoice.Scheduler(constraints)
-#     best_shows = decision_maker.make_decision(cine_cal.events())
-#     # force_scrape=False, load_IMDb_directors=False, assign_ratings=False)    
-#     print best_shows
-#     # cine_cal.write_cvs("summer_films.cvs")
+    constraints = scheduler.SchedulerConstraints()
+#    constraints.add_rating_function(rating_assigner.rate_one)
+#    constraints.add_time_constraints(events)
+    constraints.set_max_activity_by_day = 1
+    constraints.set_max_activity_by_week = 3
+    constraints.add_activity_to_avoid("")
+    constraints.add_activity_to_perfom("")
+     
+    # Get the best cinematheque_shows with my agenda and decision params
+    decision_maker = scheduler.Scheduler(constraints)
+    best_shows = decision_maker.make_decision(rated_seances)
+    print best_shows
+    
+    # personal_calendar.push_events(best_shows)
+    # personal_calendar.writeCVS("Cinematheque_films.cvs")
     
 __main__()
